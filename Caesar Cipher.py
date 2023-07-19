63d155ab6826d45138cac15db41119aeb5713815
Connor MacNeil <50525187+WobblyRonnoc@users.noreply.github.com>
2023-07-19 10:52:06 -0400

    cleaned up menu

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -4,9 +4,10 @@ substitution_ciper_a = {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g
 substitution_ciper_b = {0 : 'a', 1 : 'b', 2 : 'c', 3 : 'd', 4 : 'e', 5 : 'f', 6 : 'g', 7 : 'h', 8 : 'i', 9 : 'j', 10 : 'k', 11 : 'l', 12 : 'm', 13 : 'n', 14 : 'o', 15 : 'p', 16 : 'q', 17 : 'r', 18 : 's', 19 : 't', 20 : 'u', 21 : 'v', 22 : 'w', 23 : 'x', 24 : 'y', 25 : 'z'}
 
 def get_choice():
-    choice = input("---------------------------------------------------------------\nWhat would you like to do? (Press enter to continue) \n1. Encrypt a message \n2. Decrypt a message \n3. Crack \nEnter your choice: ")
+    choice = input("---------------------------------------------------------------\nWhat would you like to do? (Press enter to continue) \n1. Encrypt a message \n2. Decrypt a message \n3. Crack \nAny other input will exit. \nEnter your choice: ")
     return choice
 
+# returns a different message input promt depending on the choice
 def get_message(option):
     if option == "1":
         message = input("Enter the message to encrypt: ").lower()
@@ -19,9 +20,9 @@ def get_message(option):
     return message
 
 def get_key():
-    key = int(input("Enter the shift cipher: "))
+    key = int(input("Enter the key: "))
     while key < 0 or key > 25:
-        print("Invalid shift cipher. Please enter a number between 0 and 25")
+        print("Invalid key. Please enter a number between 0 and 25")
         key = int(input("Enter the key: "))
     return key
 
@@ -31,15 +32,16 @@ def get_key():
 
 # Encrypt the message(x) using key(k)
 def encrypt(x, k):
-    message_list = [*x] #/ *x splits the string into a list of letters
+    message_list = [*x] #/ split the string into a list of letters
     encrypted_message = [] # empty list to store the encrypted message
-    
+    ciper_text = [] # empty list to store the encrypted message as letters
+
+
     for i in range(len(message_list)):
         substituted_letter = substitution_ciper_a[message_list[i]]  # substitute each letter in the message with the corresponding letter in the dictionary
         Y = (substituted_letter + k) % 26 # add the key to the substituted letter and mod 26
         encrypted_message.append(Y) # append the encrypted letter to the empty list
         
-    ciper_text = [] # empty list to store the encrypted message as letters
     
     for i in range(len(encrypted_message)):
         encrypted_message[i] = substitution_ciper_b[encrypted_message[i]]
@@ -49,25 +51,29 @@ def encrypt(x, k):
 
 # Decrypt the message(x) using key(k) 
 def decrypt(x, k):
-    message_list = [*x] #/ *x splits the string into a list of letters
+    message_list = [*x] # split the string into a list of letters
     decrypted_message = [] # empty list to store the decrypted message
-    
+    plain_text = [] # empty list to store the decrypted message as letters 
+
+    # keep the space in the decrypted message
     for i in range(len(message_list)):
-        if message_list[i] == ' ': # keep the space in the decrypted message
+        if message_list[i] == ' ': 
             decrypted_message.append(message_list[i]) 
             continue        
-        substituted_letter = substitution_ciper_a[message_list[i]]  # substitute each letter in the message with the corresponding letter in the dictionary
-        Y = (substituted_letter - k) % 26 # subtract the key to the substituted letter and mod 26
-        decrypted_message.append(Y) # append the decrypted letter to the empty list
         
-    plain_text = [] 
+        substituted_letter = substitution_ciper_a[message_list[i]] 
+        Y = (substituted_letter - k) % 26 
+        decrypted_message.append(Y) 
+        
     
+    # keep the space in the decrypted message
     for i in range(len(decrypted_message)):
         if decrypted_message[i] == ' ':
-            plain_text.append(decrypted_message[i]) # keep the space in the decrypted message
-            continue
-        decrypted_message[i] = substitution_ciper_b[decrypted_message[i]]
-        plain_text.append(decrypted_message[i])
+            plain_text.append(decrypted_message[i])
+            continue 
+        
+        decrypted_message[i] = substitution_ciper_b[decrypted_message[i]] 
+        plain_text.append(decrypted_message[i]) 
         
     return plain_text 
 
@@ -78,6 +84,7 @@ def crack(message):
         print(f"Key: {i}, Message: {''.join(decrypt(message, i))}")
         
 
+# Main program
 while True:
     choice = get_choice()
     if choice == "1":
